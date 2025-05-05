ec1bb9290209a15a17ac61d97445b111aa39d77b
Arthur <89831445+arthuwu@users.noreply.github.com>
2025-05-05 22:20:43 +0100

    feat: update scroll logic

--- src/components/scrollToHash.tsx
+++ src/components/scrollToHash.tsx
@@ -6,25 +6,30 @@ const ScrollToHash = () => {
 
   useEffect(() => {
     const hash = location.hash?.slice(1);
-    if (!hash) return;
 
-    let attempts = 0;
-    const maxAttempts = 5;
     const headerOffset = window.innerHeight * 0.06; //6vh
 
-    const scrollToElement = () => {
-      const el = document.getElementById(hash);
-      if (el) {
-        const y =
-          el.getBoundingClientRect().top + window.scrollY - headerOffset;
-        window.scrollTo({ top: y, behavior: "smooth" });
-      } else if (attempts < maxAttempts) {
-        attempts++;
-        setTimeout(scrollToElement, 100);
-      }
-    };
+    if (hash) {
+      let attempts = 0;
+      const maxAttempts = 5;
 
-    scrollToElement();
+      const scrollToElement = () => {
+        const el = document.getElementById(hash);
+        if (el) {
+          const y =
+            el.getBoundingClientRect().top + window.scrollY - headerOffset;
+          window.scrollTo({ top: y, behavior: "smooth" });
+        } else if (attempts < maxAttempts) {
+          attempts++;
+          setTimeout(scrollToElement, 100);
+        }
+      };
+
+      scrollToElement();
+    } else {
+      // scroll to top if no hash
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
   }, [location]);
 
   return null;
